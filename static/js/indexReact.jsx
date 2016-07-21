2a5fe228a0f66463f4da4cc1715a35ce6e10ca47
uiandwe <uiandwe@gmail.com>
2016-07-21 20:58:08 +0900

    react data list

--- static/js/indexReact.jsx
+++ static/js/indexReact.jsx
@@ -1,7 +1,14 @@
+var bands = [
+    {name: "band1", "image": "http://www.gifpng.com/300/?text=한국어&line-height=2.5"},
+{name: "band2", "image": "http://www.gifpng.com/300/?text=English&line-height=2.5"},
+{name: "band3", "image": "http://www.gifpng.com/300/?text=한국어&line-height=2.5"}
+]
+
+
 var BandComponent = React.createClass({
 
     getInitialState: function(){
-      return {customText: "fuck"};
+      return {customText: "test"};
     },
     customClickFuntion: function(){
       this.setState({customText:"click"});
@@ -12,14 +19,29 @@ var BandComponent = React.createClass({
             <div className="testClass" >
                 <h1>{this.state.customText}</h1>
                 <button onClick={this.customClickFuntion}>click me</button>
+                {bands.map(function(band){
+                    return(
+                    <Band name={band.name} image={band.image} />
+                    )
+                })}
 
             </div>
         )
     }
 });
 
+var Band = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <img src={this.props.image} />
+            </div>
+        )
+    }
+});
 
 React.render(
-    <BandComponent />,
+    <BandComponent bands={bands} />,
     document.getElementById("band_component")
 )
