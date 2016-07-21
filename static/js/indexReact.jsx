2b878a7d86ccdd8e90dea07d1345f5b864013674
uiandwe <uiandwe@gmail.com>
2016-07-21 20:36:31 +0900

    react event state

--- static/js/indexReact.jsx
+++ static/js/indexReact.jsx
@@ -1,15 +1,19 @@
 var BandComponent = React.createClass({
 
+    getInitialState: function(){
+      return {customText: "fuck"};
+    },
+    customClickFuntion: function(){
+      this.setState({customText:"click"});
+    },
     render: function(){
 
         return(
             <div className="testClass" >
+                <h1>{this.state.customText}</h1>
+                <button onClick={this.customClickFuntion}>click me</button>
 
-                <div>test</div>
-                <div>test</div>
-                <div>test</div>
-
-                </div>
+            </div>
         )
     }
 });
